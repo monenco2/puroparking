358f5a4e670ff1f5b26f63bd794ddea6ed243d75
monenco2 <keinersmithherreragalvis@jesusamigo.com.co>
2025-06-16 13:28:15 -0500

    modelos subidos

--- backend/apps/vehiculo/models.py
+++ backend/apps/vehiculo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+Placa = models.TextField("Placa")
+matricula_vehiculo = models.TextField("Matricula y vehiculo")
+usuario_id = models.TextField("Usuario")
+fecha_ingreso = models.TextField("Fecha de ingreso")
+Servicio_id = models.TextField("Servicio")
+
